b7185749cf6abd2cbac1bbac3a627f95514ec79f
murillogy <Picodaplantadearroz@gmail.com>
2023-04-12 22:52:21 -0300

    Foram adicionados mais exercícios.

--- Python 3 – Mundo 2/exercicios/ex061.py
+++ Python 3 – Mundo 2/exercicios/ex061.py
@@ -1,12 +1,21 @@
 #  Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA,
 #  mostrando os 10 primeiros termos da progressão usando a estrutura while.
 
-n = int(input('Qual o primeiro termo da PA? '))
-razao = int(input('Qual a sua razao? '))
-c = 0
-pa = 0
-while c != 10:
-    c = c + 1
-    pa = n + (c - 1) * razao
-    print(pa, end=" -> ")
-print('FIM')
+a = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+n = 1
+t = 0
+limite = 11
+novoLimite = 0
+while n <= limite:
+    x = a + (n - 1) * r
+    print(x, end=" -> ")
+    n = n + 1
+    t = t + 1
+    if n == limite:
+        print('PAUSA')
+        novoLimite = int(input('Quantos termos você quer mostrar a mais? '))
+        if novoLimite == 0:
+            print(f'Progressão aritmética finalizada com {t} termos mostrados.')
+            limite = 0
+        limite = novoLimite + limite

e2f99b4618ecb25e98587da6c4a2d435a8403762
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-05-01 19:14:54 -0300

    Exercícios do mundo 3 falando sobre Tuplas.

--- Python 3 – Mundo 3/exercícios/ex077.py
+++ Python 3 – Mundo 3/exercícios/ex077.py
@@ -1,10 +1,18 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
-tupla = ('Eu', 'amo', 'o', 'Sales', 'Bowen', 'Berloffa')
+tupla = ('eu', 'amo', 'o', 'Sales', 'Bowen', 'Berloffa')
 
 for c in range(0, len(tupla)):
     print(f'\nNa palavra {tupla[c]} temos:', end=' ')
 
-    if tupla[c] in 'AEIOU':
-        print(tupla[c].split())
+    if "a" in tupla[c]:
+        print("a", end=' ')
+    if "e" in tupla[c]:
+        print("e", end=' ')
+    if "i" in tupla[c]:
+        print("i", end=' ')
+    if "o" in tupla[c]:
+        print("o", end=' ')
+    if "u" in tupla[c]:
+        print("u", end=' ')
